1915944054c99478d756a465de21550e131b81ff
Nurislam Ab <you@example.com>
2020-09-30 18:42:29 +0500

    Update configs for the phaser

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -6,4 +6,12 @@ export default {
   width: 1280,
   height: 600,
   autoCenter: Phaser.Scale.CENTER_BOTH,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: true,
+    },
+  },
 };
