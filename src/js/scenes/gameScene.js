b1692e0855a6c76ffb867c07cce8bba325b617a9
Nurislam Ab <you@example.com>
2020-10-01 17:44:31 +0500

    Add update scene method for the game scene

--- src/js/scenes/gameScene.js
+++ src/js/scenes/gameScene.js
@@ -38,4 +38,22 @@ export default class GameScene extends Phaser.Scene {
     this.cameras.main.startFollow(this.player);
     this.cameras.main.roundPixels = true;
   }
+
+  update() {
+    this.player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (this.cursors.left.isDown) {
+      this.player.body.setVelocityX(-80);
+    } else if (this.cursors.right.isDown) {
+      this.player.body.setVelocityX(80);
+    }
+
+    // Vertical movement
+    if (this.cursors.up.isDown) {
+      this.player.body.setVelocityY(-80);
+    } else if (this.cursors.down.isDown) {
+      this.player.body.setVelocityY(80);
+    }
+  }
 }
