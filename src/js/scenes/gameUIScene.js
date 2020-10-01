e9f083fb672119f7035a1715cdeb267d41619233
Nurislam Ab <you@example.com>
2020-10-01 20:38:29 +0500

    Add game scene HUD

---
+++ src/js/scenes/gameUIScene.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+
+class GameUIScene extends Phaser.Scene {
+  constructor() {
+    super('GameUI');
+  }
+
+  preload() {
+  }
+
+  create() {
+    this.graphics = this.add.graphics();
+    this.graphics.lineStyle(1, 0xffffff);
+    this.graphics.fillStyle(0x031f4c, 1);
+    this.graphics.strokeRect(2, 150, 90, 100);
+    this.graphics.fillRect(2, 150, 90, 100);
+    this.graphics.strokeRect(95, 150, 90, 100);
+    this.graphics.fillRect(95, 150, 90, 100);
+    this.graphics.strokeRect(188, 150, 130, 100);
+    this.graphics.fillRect(188, 150, 130, 100);
+  }
+}
+
+export default GameUIScene;
