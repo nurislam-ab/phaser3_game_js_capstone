fa882c680230cefbb256efeef7503430dd452bcd
Nurislam Ab <you@example.com>
2020-10-02 15:02:24 +0500

    Add menus to the game ui scene

--- src/js/scenes/gameUIScene.js
+++ src/js/scenes/gameUIScene.js
@@ -1,4 +1,7 @@
 import Phaser from 'phaser';
+import HeroesMenu from '../game_hud_components/heroesMenu';
+import ActionsMenu from '../game_hud_components/actionsMenu';
+import EnemiesMenu from '../game_hud_components/enemiesMenu';
 
 class GameUIScene extends Phaser.Scene {
   constructor() {
@@ -18,6 +21,27 @@ class GameUIScene extends Phaser.Scene {
     this.graphics.fillRect(95, 150, 90, 100);
     this.graphics.strokeRect(188, 150, 130, 100);
     this.graphics.fillRect(188, 150, 130, 100);
+
+    // basic container to hold all menus
+    this.menus = this.add.container();
+
+    this.heroesMenu = new HeroesMenu(195, 153, this);
+    this.actionsMenu = new ActionsMenu(100, 153, this);
+    this.enemiesMenu = new EnemiesMenu(8, 153, this);
+
+    // the currently selected menu
+    this.currentMenu = this.actionsMenu;
+
+    // add menus to the container
+    this.menus.add(this.heroesMenu);
+    this.menus.add(this.actionsMenu);
+    this.menus.add(this.enemiesMenu);
+
+    this.battleScene = this.scene.get('BattleScene');
+  }
+
+  remapHeroes() {
+    
   }
 }
 
