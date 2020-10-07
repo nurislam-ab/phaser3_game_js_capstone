b6ac1539334edfe8bbed3b50ec24de836f3f0c81
Nurislam Ab <you@example.com>
2020-10-07 21:42:19 +0500

    Fix linter offences

--- src/js/tests/score.test.js
+++ src/js/tests/score.test.js
@@ -1,13 +1,13 @@
-import Score from '../objects/score';
+import { incScore, getScore, resetScore } from '../objects/score';
 
 describe('Testing Score model', () => {
   test('increments score', () => {
-    expect(Score.incScore(20)).toBe('20');
+    expect(incScore(20)).toBe('20');
   });
   test('returns score 20', () => {
-    expect(Score.getScore()).toBe(20);
+    expect(getScore()).toBe(20);
   });
   test('resets the score equal to zero', () => {
-    expect(Score.resetScore()).toBe('0');
+    expect(resetScore()).toBe('0');
   });
 });
--- src/js/tests/scoreApi.test.js
+++ src/js/tests/scoreApi.test.js
@@ -1,14 +1,14 @@
-import ScoreApi from '../api/scoreApi';
+import { postData, getData } from '../api/scoreApi';
 
 describe('Test score api', () => {
   test('returns the post request response for succesfull sending data to the data api', () => {
-    ScoreApi.postData('Adam', 30).then(data => {
+    postData('Adam', 30).then(data => {
       expect(data).toBe('Leaderboard score created correctly.');
     });
   });
 
   test('gets the data from the leaderboard api and renders it', () => {
-    ScoreApi.getData().then(data => {
+    getData().then(data => {
       expect(typeof data).toBe('object');
     });
   });
